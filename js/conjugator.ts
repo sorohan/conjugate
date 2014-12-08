b00fcf1b4990a5f625d99112608412ab5189dfa3
Ben Sorohan <ben@backbase.com>
2014-12-08 22:01:44 +0100

    Started adding of support for irregular verbs.

--- js/conjugator.ts
+++ js/conjugator.ts
@@ -1,96 +1,103 @@
 
 class Conjugation {
-	conjugations: { [tense: string]: TenseConjugation } = { };
-	constructor(public verb: string) { }
+    conjugations: { [tense: string]: TenseConjugation } = { };
+    constructor(public verb: string) { }
 }
 
 class TenseConjugation {
-	conjugations: { [subject: string]: SubjectTenseConjugation } = { };
-	constructor ( public verb: string, public tense: string) { }
+    conjugations: { [subject: string]: SubjectTenseConjugation } = { };
+    constructor ( public verb: string, public tense: string) { }
 }
 
 class SubjectTenseConjugation {
-	constructor (
-		public verb: string,
-		public tense: string,
-		public subject: string,
-		public conjugatedVerb: string
-	) { }
+    constructor (
+        public verb: string,
+        public tense: string,
+        public subject: string,
+        public conjugatedVerb: string
+    ) { }
 }
 
 interface ConjugatorInterface {
-	language: string;
-	rulesets: { [tense: string]: TenseRulesetInterface };
-	addTenseRuleset: (ruleset: TenseRulesetInterface) => void;
-	conjugate: (verb: string) => Conjugation;
+    language: string;
+    irregulars: Conjugation[];
+    rulesets: { [tense: string]: TenseRulesetInterface };
+    addTenseRuleset: (ruleset: TenseRulesetInterface) => void;
+    addIrregulars: (irregulars: Conjugation[]) => void;
+    conjugate: (verb: string) => Conjugation;
 }
 
 interface TenseRulesetInterface {
-	tense: string;
-	rules: { [subject: string]: TenseRuleInterface };
-	conjugate: (verb: string) => TenseConjugation;
+    tense: string;
+    rules: { [subject: string]: TenseRuleInterface };
+    conjugate: (verb: string) => TenseConjugation;
 }
 
 interface TenseRuleInterface {
-	tense: string;
-	subject: string;
-	fn: ConjugateFunc;
-	conjugate: (verb: string) => SubjectTenseConjugation;
+    tense: string;
+    subject: string;
+    fn: ConjugateFunc;
+    conjugate: (verb: string) => SubjectTenseConjugation;
 }
 
 // conjugation function, takes an infinitive verb string and returns a conjugated verb string.
 interface ConjugateFunc {
-	(verb: string): string;
+    (verb: string): string;
 }
 
 class Conjugator implements ConjugatorInterface {
-	rulesets : { [tense: string]: TenseRuleset } = {};
+    rulesets : { [tense: string]: TenseRuleset } = {};
+    irregulars: Conjugation[];
 
-	constructor(public language: string) {
-	}
+    constructor(public language: string) {
+    }
 
-	addTenseRuleset(ruleset: TenseRuleset) {
-		this.rulesets[ruleset.tense] = ruleset;
-	}
+    addTenseRuleset(ruleset: TenseRuleset) {
+        this.rulesets[ruleset.tense] = ruleset;
+    }
 
-	conjugate(verb: string) {
-		var conjugation = new Conjugation(verb);
-		var self = this;
+    conjugate(verb: string) {
+        var conjugation = new Conjugation(verb);
+        var self = this;
 
-		Object.keys(self.rulesets).forEach(function(tense: string) {
-			conjugation.conjugations[tense] = self.rulesets[tense].conjugate(verb);
-		});
+        Object.keys(self.rulesets).forEach(function(tense: string) {
+            conjugation.conjugations[tense] = self.rulesets[tense].conjugate(verb);
+        });
 
-		return conjugation;
-	}
+        return conjugation;
+    }
+
+    addIrregulars(irregulars: Conjugation[]) {
+        this.irregulars = irregulars;
+    }
 };
 
 class TenseRuleset implements TenseRulesetInterface {
-	rules: { [subject: string]: TenseRuleInterface } = {};
+    rules: { [subject: string]: TenseRuleInterface } = {};
 
-	constructor(public tense: string) {
-	}
+    constructor(public tense: string) {
+    }
 
-	addTenseRule(rule: TenseRule) {
-		this.rules[rule.subject] = rule;
-	}
+    addTenseRule(rule: TenseRule) {
+        this.rules[rule.subject] = rule;
+    }
 
-	conjugate(verb: string) {
-		var tenseConjugation = new TenseConjugation(verb, this.tense);
-		var self = this;
+    conjugate(verb: string) {
+        var tenseConjugation = new TenseConjugation(verb, this.tense);
+        var self = this;
 
-		Object.keys(self.rules).forEach(function(subject: string) {
-			tenseConjugation.conjugations[subject] = self.rules[subject].conjugate(verb);
-		});
+        Object.keys(self.rules).forEach(function(subject: string) {
+            tenseConjugation.conjugations[subject] = self.rules[subject].conjugate(verb);
+        });
 
-		return tenseConjugation;
-	}
+        return tenseConjugation;
+    }
 }
 
 class TenseRule implements TenseRuleInterface {
-	constructor(public tense: string, public subject: string, public fn: ConjugateFunc) { }
+    constructor(public tense: string, public subject: string, public fn: ConjugateFunc) { }
 
-	conjugate(verb: string) {
-		return this.fn.call(this, verb, this.tense, this.subject);
-	}
+    conjugate(verb: string) {
+        return this.fn.call(this, verb, this.tense, this.subject);
+    }
 }
