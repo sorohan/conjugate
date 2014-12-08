02bce4e22a5b385c1b713121381a8600364c6405
Ben Sorohan <ben@backbase.com>
2014-12-08 08:55:54 +0100

    added a bunch more tests.

--- js/tests/tests.ts
+++ js/tests/tests.ts
@@ -1,43 +1,88 @@
 
 /// <reference path="../vendor/jasmine/jasmine.d.ts" />
 
-describe("Testing Netherlands Conjugation", function() {
+describe("Testing Netherlands Language Helper", function() {
     it("checks vowel length", function() {
-        expect(NlHelper['isLongVowel']('mak')).toBe(false);
         expect(NlHelper['isLongVowel']('werk')).toBe(false);
         expect(NlHelper['isLongVowel']('wer')).toBe(false);
+        expect(NlHelper['isLongVowel']('mak')).toBe(false);
     });
 
     it("splits the word into syllables", function() {
-        var syllables;
+        // @see http://www.dutchgrammar.com/en/?n=SpellingAndPronunciation.05
 
-        syllables = NlHelper['syllableExplode']('werken');
-        expect(utils.isEqual(syllables, ['wer','ken'])).toBe(true);
+        // Rule I.
+        expect(utils.isEqual(NlHelper['syllableExplode']('moeten'), ['moe','ten'])).toBe(true);
+        expect(utils.isEqual(NlHelper['syllableExplode']('maken'), ['ma','ken'])).toBe(true);
+        expect(utils.isEqual(NlHelper['syllableExplode']('zeuren'), ['zeu','ren'])).toBe(true);
 
-        syllables = NlHelper['syllableExplode']('moeten');
-        expect(utils.isEqual(syllables, ['moe','ten'])).toBe(true);
+        // Rule II.
+        expect(utils.isEqual(NlHelper['syllableExplode']('paarden'), ['paar','den'])).toBe(true);
+        expect(utils.isEqual(NlHelper['syllableExplode']('dingen'), ['din','gen'])).toBe(true);
+        expect(utils.isEqual(NlHelper['syllableExplode']('wennen'), ['wen','nen'])).toBe(true);
+        expect(utils.isEqual(NlHelper['syllableExplode']('venster'), ['ven','ster'])).toBe(true);
+        expect(utils.isEqual(NlHelper['syllableExplode']('enclave'), ['en','cla', 've'])).toBe(true);
+        expect(utils.isEqual(NlHelper['syllableExplode']('obstinaat'), ['ob','sti', 'naat'])).toBe(true);
 
-        syllables = NlHelper['syllableExplode']('paarden');
-        expect(utils.isEqual(syllables, ['paar','den'])).toBe(true);
+        // TODO: Rule III.
+        // TODO: Rule IV.
+    });
 
-        syllables = NlHelper['syllableExplode']('obstinaat');
-        expect(utils.isEqual(syllables, ['ob','sti', 'naat'])).toBe(true);
+    it("gets the crude stem", function() {
+        // infinitive, crude stem, stem.
+        var examples = [
+            [ 'maken', 'mak', 'maak' ],
+            [ 'nemen', 'nem', 'neem' ],
+            [ 'lopen', 'lop', 'loop' ],
+            [ 'leren', 'ler', 'leer' ],
+            [ 'koken', 'kok', 'kook' ],
+            [ 'breken', 'brek', 'breek' ],
+            [ 'vuren', 'vur', 'vuur' ],
+            [ 'horen', 'hor', 'hoor' ],
+            [ 'weten', 'wet', 'weet' ]
+        ];
+
+        examples.forEach(function(item) {
+            expect(NlHelper['getCrudeStem'](item[0])).toBe(item[1]);
+        });
     });
 
     it("lengthens a vowel stem", function() {
-        var stem = NlHelper['longVowelStem']('werken', 'werk');
-        expect(stem).toBe('werk');
+        // infinitive, crude stem, stem.
+        var examples = [
+            [ 'maken', 'mak', 'maak' ],
+            [ 'lopen', 'lop', 'loop' ],
+            [ 'leren', 'ler', 'leer' ],
+            [ 'koken', 'kok', 'kook' ],
+            [ 'breken', 'brek', 'breek' ],
+            [ 'vuren', 'vur', 'vuur' ],
+            [ 'horen', 'hor', 'hoor' ],
+            [ 'weten', 'wet', 'weet' ]
+        ];
+
+        examples.forEach(function(item) {
+            expect(NlHelper['longVowelStem'](item[0], item[1])).toBe(item[2]);
+        });
     });
 
     it("gets the regular verb stem", function() {
-        var stem = NlHelper['getStem']('werken');
-        expect(stem).toBe('werk');
+        // infinitive, crude stem, stem.
+        var examples = [
+            [ 'werken', 'werk' ]
+        ];
+
+        examples.forEach(function(item) {
+            expect(NlHelper['getStem'](item[0])).toBe(item[1]);
+        });
     });
 
     it("gets the verb stem for a long vowel", function() {
         var stem = NlHelper['getStem']('maken');
         expect(stem).toBe('maak');
     });
+});
+
+describe("Testing Netherlands Conjugator", function() {
 
     it("conjugates werken present tense", function() {
         var dutchConjugator = new NetherlandsConjugator();
